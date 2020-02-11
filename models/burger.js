dcf32c6548755a4c47583f867c44b625bf96d74f
Joe Bailey <baileyrva@gmail.com>
2020-02-10 20:03:52 -0500

    testing sequlize setup of database

---
+++ models/burger.js
@@ -0,0 +1,7 @@
+module.export = function(sequelize, DataTypes) {
+    let burger = sequelize.define("burger", {
+        burger_name: DataTypes.STRING,
+        devoured: DataTypes.BOOLEAN
+    });
+    return burger; 
+}
